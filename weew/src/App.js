2eb532db5bbac800b90199f5bddd83d681b8c4d2
Ismael Medrano <ismaelmed500@gmail.com>
2023-08-30 10:58:45 -0300

    hecho por sem1312

--- weew/src/App.js
+++ weew/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
-import camavinga from "./camavinga.jpg"
+import React, { useState } from 'react';
+import camavinga from "./camavinga.jpg";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 
 function App() {
+
+  const [input,setInput] = useState();
+
+  function handleInput(event) {
+    setInput(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1> CAMAVINGA ONLINE WEB SERVER PAGE</h1>
-        <img src={camavinga} alt="Camavinga" />
+        <h1> CLASH OF CLANS SKILL-CHECK</h1>
+        <p> INGRESE SU ID:</p>
+        <input onChange = {handleInput}></input> 
+        <p> SU ID: </p>
+        <p>{input}</p>
+        <img style={{ width: '1px', height: '1px' }} src={camavinga} alt="Camavinga" />
+
       </header>
     </div>
   );
