0068287f03397428a35e271af52eaa86dd44e005
PATRICIO GALLO DILLON <d47351717@alumnos.itr.edu.ar>
2023-09-01 11:34:44 -0300

    sio

--- weew/src/clashapi.js
+++ weew/src/clashapi.js
@@ -1,5 +1,5 @@
     
-        axios.get('https://api.example.com/data')
+        axios.get('Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjhkNjVhYTE5LWFiOWQtNDU2My1iNTg1LTcwZTIwODY2NGY3ZSIsImlhdCI6MTY5MzQwNDQ4NSwic3ViIjoiZGV2ZWxvcGVyL2IwMmMxYTE5LTk0MTQtOWRjMS1lMjZhLTkzMzAxYTNlYmI1NyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjAuMC4wLjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.m7MohzEtld5N7T9XkBVQJ9RDtuGDsGQjPHaxQJJpXEk5Kw_lysU12KtlS5de4EqQ1feU9dHNhoGSIox3lHsIdw https://api.clashofclans.com/v1/players/%2392v2r2lvq')
         .then(response => {
          
           console.log(response.data);
