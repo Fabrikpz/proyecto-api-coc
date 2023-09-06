8215948e821e3a50ec5a6ada2e464f0c104e880a
Fabrikpz <fabrisilveyra@gmail.com>
2023-09-06 14:17:47 -0300

    ya vere in ma house

--- weew/src/Home.js
+++ weew/src/Home.js
@@ -10,11 +10,30 @@ function App() {
   const handleInput = (event) => {
     setInput(event.target.value);
   }
-
-  const fetchData = async () => {
+  const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjEzNWI1NDFiLTQ1OTAtNDk1OC1hZTdiLWQ5ZmEwOWE2MGViZiIsImlhdCI6MTY5NDAwOTI3Niwic3ViIjoiZGV2ZWxvcGVyLzJhYWViMmU1LTJkMWYtZWMyZC01MTVlLTFhNmI4MjAzNTk1MiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMC4wLjAuMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.TiT6rPdMtJNlRXI7JmcJi42_6MK4WDJBBjuoxTxbNDhVWRMalBXWMpQoz_8jEVDYQC2SnC--gF1hu5XL_Dm3Vg';
+  /*const fetchData = async () => {
     try {
       const response = await axios.get(`http://localhost:3500/getClashOfClansData/9ry99pruq`);
       setDataProfile(response.data); // Aquí asignas los datos a tu estado
+    } catch (error) {
+      console.error(`Error obteniendo data: ${error}`);
+    }
+  };*/
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3500/getClashOfClansData/${input}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${apiKey}`
+        }
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setDataProfile(data);
+      } else {
+        console.error(`Error obteniendo data: ${response.statusText}`);
+      }
     } catch (error) {
       console.error(`Error obteniendo data: ${error}`);
     }
@@ -22,12 +41,12 @@ function App() {
 //#qvyuvrpup
   return (
     <div>
-      <h1>9ry99pruq </h1>
+      <h1>qvyuvrpup</h1>
       <h1>CLASH OF CLANS STATS</h1>
       <p>INGRESE SU ID:</p>
-      <input onChange={handleInput} value={input}></input>
+      <input onChange={handleInput} value={input} placeholder="enter your id"></input>
       <button onClick={fetchData}>Get Data</button>
-      {dataProfile.name && <h1>La skill de {dataProfile.name}</h1>}
+      {dataProfile.name && <h1>Username: {dataProfile.name}</h1>}
       <img src={camavinga} alt="Camavinga" />
       
 
--- weew/src/server.js
+++ weew/src/server.js
@@ -7,23 +7,23 @@ const port = 3500;
 app.use(express.json());
 app.use(cors());
 
-app.get('/getClashOfClansData/9ry99pruq', async (req, res) => {
+app.get('/getClashOfClansData/qvyuvrpup', async (req, res) => {
     const playerTag = req.params.playerTag;
-    const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjEzNWI1NDFiLTQ1OTAtNDk1OC1hZTdiLWQ5ZmEwOWE2MGViZiIsImlhdCI6MTY5NDAwOTI3Niwic3ViIjoiZGV2ZWxvcGVyLzJhYWViMmU1LTJkMWYtZWMyZC01MTVlLTFhNmI4MjAzNTk1MiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMC4wLjAuMCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.TiT6rPdMtJNlRXI7JmcJi42_6MK4WDJBBjuoxTxbNDhVWRMalBXWMpQoz_8jEVDYQC2SnC--gF1hu5XL_Dm3Vg';
-    const apiUrl = `https://api.brawlstars.com/v1/players/%239ry99pruq`;
+    const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjRmZDZiMWIxLTg1M2MtNDQ3Mi05OWI5LTYwMTNjMDQ3MmE2NyIsImlhdCI6MTY5NDAwODc3NCwic3ViIjoiZGV2ZWxvcGVyL2IxYjgyMzgyLTIxODAtNjVlNS0zZjRhLTk3N2RmNTdkNDg5NCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjAuMC4wLjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.Bwa4_X6dbHzhU4H8PgehoZEX-XEkfEtNtzND3eNyrv_TL1eleENDB_FwFRfFkicWq8V41pSEIJft0O8z46BTIA';
+    const apiUrl = `https://api.clashofclans.com/v1/players/%23${playerTag}`;
 
     try {
         const response = await axios.get(apiUrl, {
             headers: {
-                'Authorization': `Bearer ${apiKey}`
+                'Authorization': `Bearer${apiKey}`
             }
         });
 
         res.send(response.data);
         res.json(response.data);
     } catch (error) {
-        console.error(`Error obteniendo datos de Clash of Clans: ${error.message}`);
-        res.status(500).json({ error: 'Error obteniendo datos' });
+        console.error(`Error obtaining info from API: ${error.message}`);
+        res.status(500).json({ error: 'Error obtaining user info' });
     }
 });
 
